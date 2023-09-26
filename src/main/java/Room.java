074c84aab291be0d15baa8e98ac6ed894e97bc78
Jesper Deetho Jessen <jeje0002@stud.kea.dk>
2023-09-26 10:15:03 +0200

    delopgave 1 færdig

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,97 @@
+import java.util.Random;
+
 public class Room {
+    private String name;
+    private String description;
+    private Room north;
+    private Room south;
+    private Room east;
+    private Room west;
+
+    public Room(String n) {
+        this.name = n;
+        this.description = generateRandomDescription();
+    }
+
+    private String generateRandomDescription() {
+        String[] adjectives = {"dark", "spooky", "bright", "airy", "moist", "mysterious"};
+        String[] furnishings = {"with an old wooden chair", "with a broken table", "with a flickering lantern", "with a tattered rug"};
+
+        Random random = new Random();
+
+        String adjective = adjectives[random.nextInt(adjectives.length)];
+        String furnishing = furnishings[random.nextInt(furnishings.length)];
+
+        return "A " + adjective + " room " + furnishing;
+    }
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public void setNorth(Room north) {
+        if(this.north != north) {
+            this.north = north;
+            north.setSouth(this);  // To make sure the connection is two-way
+        }
+    }
+
+
+    public Room getSouth() {
+        return south;
+    }
+
+    public void setSouth(Room south) {
+        if(this.south != south) {
+            this.south = south;
+            south.setNorth(this);  // To make sure the connection is two-way
+        }
+    }
+
+
+    public Room getEast() {
+        return east;
+    }
+
+    public void setEast(Room east) {
+        if (this.east != east) {
+            this.east = east;
+            east.setWest(this);
+        }
+    }
+
+    public void setWest(Room west) {
+        if (this.west != west) {
+            this.west = west;
+            west.setEast(this);
+        }
+    }
+
+
+
+    public Room getWest() {
+        return west;
+    }
+
+    @Override
+    public String toString() {
+        return "Room name: " + this.name + ", Description: " + this.description;
+    }
+    public String availableDirections() {
+        StringBuilder sb = new StringBuilder();
+        if (this.getEast() != null) sb.append("East ");
+        if (this.getWest() != null) sb.append("West ");
+        if (this.getNorth() != null) sb.append("North ");
+        if (this.getSouth() != null) sb.append("South ");
+        return sb.toString();
+    }
 }
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,2 +1,61 @@
+import java.util.Scanner;
+
 public class UserInterface {
+    Scanner keyboard = new Scanner(System.in);
+    Adventure adv = new Adventure();
+
+    public void start() {
+        System.out.println("""
+            Welcome to Adventure game. Please type:
+            Start
+            Help
+            Look
+            Go + Direction
+            Exit
+            """);
+
+        while (true) {
+            try {
+                String choice = keyboard.nextLine().toLowerCase().trim(); // Convert input to lowercase and trim spaces
+
+                if (choice.startsWith("go ")) {
+                    String[] parts = choice.split(" "); // Split the choice by space
+                    if (parts.length > 1) {
+                        char direction = parts[1].charAt(0);
+                        adv.goDirection(direction);
+                    }
+                } else {
+                    switch (choice) {
+                        case "start" -> {
+                            System.out.println("Program starting.....");
+                            System.out.println(adv.getCurrentRoom().toString());
+                        }
+                        case "help" -> displayHelp();
+                        case "look" -> lookAround();
+                        case "exit" -> {
+                            System.out.println("Exiting Program -------- ");
+                            System.exit(0);
+                        }
+                        default -> System.out.println("Invalid choice. Please try again.");
+                    }
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+        private void displayHelp() {
+            System.out.println("Here are some helpful instructions:");
+            System.out.println("1. 'Start' - Starts the game.");
+            System.out.println("2. 'Help' - Displays this help menu.");
+            System.out.println("3. 'Look' - Shows details about the current room.");
+            System.out.println("4. 'Go + [Direction]' - Moves you in the specified direction. Valid directions are N, S, E, W.");
+            System.out.println("5. 'Exit' - Exits the game.");
+        }
+    private void lookAround() {
+        Room currentRoom = adv.getCurrentRoom();
+        System.out.println("You are in: " + currentRoom.toString());
+        System.out.println("You can go: " + currentRoom.availableDirections());
+    }
 }
