4d2322a6cdf8e98ac040dfc9d1199431e31e2fd3
Jesper Deetho Jessen <jeje0002@stud.kea.dk>
2023-10-03 13:19:26 +0200

    delopgave 1 færdig. Koden refactoret
    delopgave 2 færdig. nye commandoer virker alle. Item klasse. Player inventory virker. Rooms har egne items og kan blive taget frem og tilbage mellem dem. Koden er refactoreret
    
    delopgave 3 færdig. Har refaktoreret og tilføjet metoder til at bruge food og andre objekter der kan betegnes som "consumables".

--- src/main/java/GameInitializer.java
+++ src/main/java/GameInitializer.java
@@ -1,13 +1,51 @@
+import java.util.ArrayList;
+
 public class GameInitializer {
-    private Room currentRoom;
+    private PlayerNavigation playerNavigation;
 
     public GameInitializer() {
         MapCreator gameMapCreator = new MapCreator();
-        currentRoom = gameMapCreator.buildMap();
+        Room currentRoom = gameMapCreator.buildMap();
+        playerNavigation = new PlayerNavigation(currentRoom, 10);
     }
 
     public Room getCurrentRoom() {
-        return currentRoom;
+        return playerNavigation.getCurrentRoom();
     }
 
+    public String goDirection(String direction) {
+        return playerNavigation.goDirection(direction);
+    }
+
+    public boolean pickUpItem(String itemName) {
+        return playerNavigation.pickUpItem(itemName);
+    }
+
+    public boolean dropItem(String itemName) {
+        return playerNavigation.dropItem(itemName);
+    }
+
+    public ArrayList<Equippable> getInventory() {
+        return playerNavigation.getInventory();
+    }
+
+    public int getHealth() {
+        return playerNavigation.getHealth();
+    }
+
+    public int getCarryingWeight() {
+        return playerNavigation.getCarryingWeight();
+    }
+
+    public int getMaxCarryWeight() {
+        return playerNavigation.getMaxCarryWeight();
+    }
+
+    public boolean isPlayerAlive() {
+        return playerNavigation.isPlayerAlive(playerNavigation.getHealth());
+    }
+
+    public boolean useFood(String foodName) {
+        return playerNavigation.useFood(foodName);
+    }
 }
